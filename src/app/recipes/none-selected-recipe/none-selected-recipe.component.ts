d6556638d249c187b145abeab49fc2647a936576
Ivan <il.muniz@live.com>
2020-09-22 23:06:37 -0400

    gitflow-feature-stash: ChildRouteChallenge
    added router outlet in recipe component

---
+++ src/app/recipes/none-selected-recipe/none-selected-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-none-selected-recipe',
+  templateUrl: './none-selected-recipe.component.html',
+  styleUrls: ['./none-selected-recipe.component.css']
+})
+export class NoneSelectedRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
