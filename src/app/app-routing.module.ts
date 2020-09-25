173ecb480772247f8d0cdcfce06b5ccf0d91dca5
Ivan <il.muniz@live.com>
2020-09-24 22:00:22 -0400

    create recipe edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NoneSelectedRecipeComponent } from './recipes/none-selected-recipe/none-selected-recipe.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -12,7 +13,9 @@ const appRoutes: Routes = [
     {
         path: 'recipes', component: RecipesComponent, children: [
             { path: '', component: NoneSelectedRecipeComponent },
-            { path: ':id', component: RecipeDetailComponent }
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent }
         ]
     },
     { path: 'shopping-list', component: ShoppingListComponent }
